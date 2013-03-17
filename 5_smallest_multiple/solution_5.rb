1034e93157f3c1abd6a07d06f925b458fc2777ed
Sven <sven.dupli@gmail.com>
2013-03-17 13:10:24 +0100

    Solved problem 5

---
+++ 5_smallest_multiple/solution_5.rb
@@ -0,0 +1,15 @@
+# = Smallest multiple
+#
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+# 2520 % (1..10) == 0
+
+i = 1
+
+while true
+  break unless (2..20).any? {|n| i % n != 0}
+  i+=1
+end
+
+puts i
